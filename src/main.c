726bbe7e8b216f16ed23f8e289cdbf282352c5b1
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2026-06-19 21:41:59 +0530

    Handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+
+  // User Input
+  char cmd[32];
+  fgets(cmd, sizeof(cmd), stdin);
+
+  
+  // Remove the trailing newline
+  cmd[strlen(cmd) - 1] = '\0';
+
+  printf("%s: command not found\n", cmd);
 
   return 0;
 }
